e510b16aff00279ebf572978cc09412c63fd7dc8
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-10-14 21:41:29 +0200

    remove old crap

--- targets/thumbinator/effect.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-void effect_next(void);
-void effect_show(uint32_t t);
--- targets/thumbinator/hsv.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void hsvtorgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v);
