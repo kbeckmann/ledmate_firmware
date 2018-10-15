932f50f7fe71d780e9a915af0a10df372497bfcc
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-10-15 19:50:52 +0200

    Remove junk

--- app/main.c
+++ app/main.c
@@ -1,24 +1,11 @@
 #include <FreeRTOS.h>
-#include <string.h>
 #include <task.h>
 
-#include "cdc_uart_bridge.h"
-#include "drivers/led.h"
-#include "drivers/max14662.h"
-#include "drivers/mcp4018t.h"
-#include "platform/uart.h"
 #include "platform/usb/usb.h"
-#if (FEAT_POWER_PROFILER == 1)
-#include "platform/usb/cdc.h"
-#include "platform/usb/hid.h"
-#include "platform/adc.h"
-#include "platform/i2c.h"
-#include "power.h"
-#endif
+#include "ledmate_bridge.h"
 #include "platform/gpio.h"
 #include "target.h"
 
-#include "ledmate_bridge.h"
 
 int _read(int fd, char *msg, int len)
 {
